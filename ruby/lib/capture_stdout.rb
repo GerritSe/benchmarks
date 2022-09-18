70a658ccdfa4f0384680e017fff0905816a64abc
Gerrit Seger <gerrit.seger@gmail.com>
2022-09-18 16:32:01 +0200

    Do not write results if interrupted

--- ruby/lib/capture_stdout.rb
+++ ruby/lib/capture_stdout.rb
@@ -1,17 +1,25 @@
 require "stringio"
 
 out = StringIO.new
+interrupted = false
 
 $stdout.define_singleton_method(:write) do |string|
   super(string)
   out << string
 end
 
-at_exit do
+Signal.trap("INT") do
+  interrupted = true
+  exit
+end
+
+at_exit do |x|
+  next if interrupted
+
   out.rewind
   output = out.read
 
-  return if output.empty?
+  next if output.empty?
 
   filename = [File.basename($0, ".rb"), "result.txt"].join("_")
   File.write(filename, output)
